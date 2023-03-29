c999978dc043b2c6d8a8da8c69c6f6ddcf5346db
bando9 <bandomega1@gmail.com>
2023-03-29 09:21:00 +0700

    menambah fitur find one categories, fitur update (masih error)

--- server-semina-bootcamp-2/app/api/v1/categories/controller.js
+++ server-semina-bootcamp-2/app/api/v1/categories/controller.js
@@ -12,6 +12,51 @@ const create = async (req, res, next) => {
     }
 };
 
+const find = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const result = await Categories.findById(id);
+
+        res.status(200).json({
+            data: result,
+        })
+    } catch(err) {
+        next(err);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        // Cara 1
+        // const checkingCategories = await Categories.findOne({ _id: id });
+
+        // if (!checkingCategories) {
+        //     return res.status(404).json({
+        //         message: 'Id categories tidak ditemukan'
+        //     })
+        // };
+
+        // checkingCategories.name = name;
+        // await checkingCategories.save();
+
+        // Cara 2
+        const result = await Categories.findByIdAndUpdate(
+            { _id: id },
+            { name },
+            { runValidators: true }
+        )
+
+        res.status(200).json({
+            data: checkingCategories,
+        })
+    } catch(err) {
+        next(err);
+    }
+}
+
 const index = async(req, res, next) => {
     try {
         const result = await Categories.find().select('_id name');
@@ -25,5 +70,7 @@ const index = async(req, res, next) => {
 
 module.exports = {
     index,
+    update,
+    find,
     create,
 }
--- server-semina-bootcamp-2/app/api/v1/categories/router.js
+++ server-semina-bootcamp-2/app/api/v1/categories/router.js
@@ -1,10 +1,14 @@
 const express = require('express').Router;
-const { create, index } = require('./controller');
+const { create, index, find, update } = require('./controller');
 
 const router = express();
 
 router.get('/categories', index)
 
 router.post('/categories', create);
+
+router.get('/categories/:id', find);
+
+router.put('/categories/:id', update);
 
 module.exports = router;
